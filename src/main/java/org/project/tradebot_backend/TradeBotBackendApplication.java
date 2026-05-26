0000e96b8ad096d12937658133bd6b7b069a8883
YOOHEESU97 <yohs135@gmail.com>
2026-05-26 16:56:31 +0900

    최초 반영

---
+++ src/main/java/org/project/tradebot_backend/TradeBotBackendApplication.java
@@ -0,0 +1,13 @@
+package org.project.tradebot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeBotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeBotBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/tradebot_backend/TradeBotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.tradebot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeBotBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
